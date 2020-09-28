b23867e13accc5d099555c9a990bebaad04f7f02
chenling <chenling@yh-commercial.com>
2020-09-29 02:28:16 +0800

    首页轮播

--- app.js
+++ app.js
@@ -1,34 +1,14 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
+    wx.request({
+      url: 'https://resources.ninghao.net/wxapp-case/db.json',
+      success:(response)=>{
+        Object.assign(this.globalData, response.data)
+        //判断页面在异步获取数据之前就调用了还是之后调用,在获取数据后调用正常显示没问题,如果在获得数据之前页面就已经调用,要手动调用一下当前页面的onLoad()
+        const currentPages = getCurrentPages()
+        if(currentPages.length !== 0){
+          currentPages[currentPages.length - 1].onLoad()
         }
       }
     })
